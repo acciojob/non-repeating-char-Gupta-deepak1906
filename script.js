32483a3b738750a8250bc453caa1b29056e3c717
Gupta-deepak1906 <147096327+Gupta-deepak1906@users.noreply.github.com>
2024-09-23 01:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	const charFreqCnt={};
+for{let char of str}{
+	//charFreqCnt[char]=(charFreqCnt[char] || 0)+1;
+	charFreqCnt[char] +=1;
+}
+	else{
+		charFreqCnt[char]=1;
+	}
+}
+for{let char of str}{
+	if(charFreqCnt[char]===1) return char;
+
+	else return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
